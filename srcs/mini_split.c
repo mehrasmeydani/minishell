af0d3c79570d40fea4a0946a990e790521ff7da3
Mehras Meydani <mehrasmeydani@gmail.com>
2025-08-18 20:18:10 +0200

    split all done (I think)+ tokenizer

--- srcs/mini_split.c
+++ srcs/mini_split.c
@@ -1,48 +1,50 @@
 #include "../header/minishell.h"
 
-int	state_set(char c, char *str, int quotes_state)
+int	state_set(char c, char *str)
 {
 	int i;
 
 	i = -1;
-	if (quotes_state)
-		return (quotes_state);
 	while (str[++i])
 		if (str[i] == c)
 			return (i + 3);
 	return (-1);
 }
 
-static ssize_t	count(const char *str)
+static void	skip(char **str, ssize_t *i)
+{
+	int	state;
+	int	prev_state;
+
+	state = 0;
+	while ((*str)[(*i)] == ' ')
+			(*str)++;
+	quotes((*str)[(*i)], &state);
+	if ((state == DOUBLE || state == SINGLE) && ++(*i))
+	{
+		while ((*str)[(*i)] && quotes((*str)[(*i)], &state) && state)
+			(*i)++;
+		(*i)++;
+	}
+	else
+	{
+		state = state_set((*str)[(*i)], "<|> ");
+		prev_state = state;
+		while ((*str)[(*i)] && state == prev_state)
+			state = state_set((*str)[++(*i)], "<|> ");
+	}
+}
+
+static ssize_t	count(char *str)
 {
 	ssize_t	i;
 	ssize_t	tokens;
-	int	state;
-	int	prev_state;
-	int	quotes_state;
 
 	i = 0;
-	state = 0;
 	tokens = 0;
-	quotes_state = 0;
 	while (str[i])
 	{
-		while (str[i] == ' ')
-			i++;
-		quotes(str[i], &quotes_state);
-		if (quotes_state && ++i)
-		{
-			while (str[i] && quotes(str[i], &quotes_state) && quotes_state)
-				i++;
-			i++;
-		}
-		else
-		{
-			state = state_set(str[i], "<|> ", quotes_state);
-			prev_state = state;
-			while (str[i] && state == prev_state)
-				state = state_set(str[++i], "<|> ", quotes_state);
-		}
+		skip(&str, &i);
 		tokens++;
 	}
 	return (tokens);
@@ -59,18 +61,32 @@ static char	**ft_free(char **in, ssize_t i)
 	return (NULL);
 }
 
-// static char	*str(char *s)
-// {
-// }
+static char	*str(char **str)
+{
+	ssize_t	i;
+	char	*out;
+
+	i = 0;
+	skip(str, &i);
+	out = ft_substr(*str, 0, i);
+	*str = *str + i;
+	return (out);
+}
 
 char	**mini_split(char *in)
 {
-	//ssize_t	i;
+	ssize_t	i;
 	char	**out;
 	ssize_t	num;
 
 	num = count(in);
 	out = (char **)calloc(num + 1, sizeof(char *));
-	ft_free(out, 0);
-	return (NULL);
+	i = -1;
+	while (++i < num)
+	{
+		out[i] = str(&in);
+		if (!out[i])
+			return (ft_free(out, 0));
+	}
+	return (out);
 }
