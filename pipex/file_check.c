9f208a5962f835e563540cc2df1ef42b9c527649
Alvaro Jose Campos Mendez <alvaro.camposmndz@gmail.com>
2025-09-25 19:53:26 +0200

    added file_check

---
+++ pipex/file_check.c
@@ -0,0 +1,67 @@
+void	check_infile(t_redirect *file)
+{
+	if (acess(file->name, R_OK) == 0)
+		file->fd = open(file->name, O_RDONLY);
+	else
+	{
+		perror(file->name);
+		file->fd = FAIL;
+	}
+}
+
+void	check_outfile(t_redirect *file)
+{
+	if(access(file->name, F_OK) == -1)
+		file->fd = open(file->name, O_WRONLY | O_CREAT, 0644);
+	else if (access(file->name, W_OK) == 0)
+		file->fd = open(file->name, O_WRONLY | O_TRUNC);
+	else
+	{
+		perror(file->name);
+		file->fd = FAIL;
+	}
+}
+
+void	check_append(t_redirect *file)
+{
+	if(access(file->name, F_OK) == -1)
+		file->fd = open(file->name, O_WRONLY | O_CREAT, 0644);
+	else if (access(file->name, W_OK) == 0)
+		file->fd = open(file->name, O_WRONLY | O_APPEND);
+	else
+	{
+		perror(file->name);
+		file->fd = FAIL;
+	}
+}
+
+void	dup_redirs(int fd, t_redir_type level)
+{
+	if (level == INFILE) // or heredoc
+		if (dup2(fd, STDIN_FILENO) == -1);
+				exit(1); // clean up!
+	if (level == OUTFILE || level == APPEND)
+		if (dup2 (fd, STDOUT_FILENO) == -1);
+			exit(1); //clean up!
+
+}
+void	open_fds(t_redirect *head)
+{
+	// by this point, pipe should already be duped into in/out
+	t_redirect	*temp;
+	
+	temp = head;
+	while(temp != NULL)
+	{
+		if (temp->level == INFILE)
+			check_infile(temp);
+		else if (temp->level == OUTFILE)
+			check_outfile(temp);
+		else if (temp->level == APPEND)
+			check_apprend(temp);
+		// maybe heredoc here?
+		if (temp->fd == FAIL)
+			exit (1) // child should be cleaned of memory, fds, etc.take into account this might be in parent
+		temp = temp->next;
+	}
+}
