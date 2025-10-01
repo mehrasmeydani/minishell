e71ad2b14f7303a743c9e0dd7b98d1e02038a8f0
Alvaro Jose Campos Mendez <alvaro.camposmndz@gmail.com>
2025-10-01 20:41:58 +0200

    added better error handling

--- pipex/child_spawning.c
+++ pipex/child_spawning.c
@@ -91,41 +91,77 @@ int	fill_struct(t_exec *exec, t_minishell *mini)
 		return(perror("pid allocation"), freepaths(exec->pathlist),
 			exec->pathlist = NULL, -1);
 	}
+	exec->pipe[0][0] = -1;
+	exec->pipe[0][1] = -1;
+	exec->pipe[1][0] = -1;
+	exec->pipe[1][1] = -1;
 	return (1);
 }
 
 void	pipe_em()
 {
+	
+}
 
+void	safe_close_fd(int *fd)
+{
+	if (*fd == -1)
+		return ;
+	close(*fd);
+	*fd = -1;
+}
+
+void	close_all_pipes(int pipes[2][2])
+{
+	safe_close_fd(&pipes[0][0]);
+	safe_close_fd(&pipes[0][1]);
+	safe_close_fd(&pipes[1][0]);
+	safe_close_fd(&pipes[1][1]);
+}
+
+void	close_exit(t_exec *exec, t_minishell *mini, char *errorstr, int ex_code)
+{
+	(void) mini; // placeholder so i can free ministuff after
+	freepaths(exec->pathlist);
+	close_all_pipes(exec->pipe);
+	if (errorstr != NULL) // null when no error, pass a str for error.
+		perror(errorstr);
+	exit(ex_code);
 }
 void	spawn_children(t_minishell *mini)
 {
 	size_t	i;
 	t_exec	exec;
-	int current;
 
-	current = 0;
 	if (fill_struct(&exec, mini) == -1)
 		return ; // cleaned, all pids closed
 	if (exec.children_count == 1) // this check should also verify if its a builtin
-		return (perror("single command logic with is not here yet, but must be implemented"));
+		perror("single command logic with builtin is not here yet, but must be implemented");
 	i = -1;
 	while(++i < exec.children_count)
 	{
-		if (pipe(exec.pipe[current]) < 0)
-			return (perror("pipe")); // fds should be closed
-		exec.pids[i] = fork();
-		// rig child here to set the rolling pipe correctly
+		if (pipe(exec.pipe[i % 2]) < 0)
+			return (perror("pipe"));
+		if ((exec.pids[i] = fork()) == -1)
+			return (perror("fork")); // i should also wait here for all the previous commands!
 		if (exec.pids[i] == 0)
 		{
-
-			// pipe correctly
+			if(i > 0)
+			{
+				if (dup2(exec.pipe[(i - 1) % 2][0], STDIN_FILENO) == -1) // if not first command, previous command to STDIN.
+					close_exit(&exec, mini, "dup STDIN", 1);
+			}
+			if (i < exec.children_count - 1)
+			{
+				if (dup2(exec.pipe[i % 2][1], STDOUT_FILENO) == -1) // if not last command, current to to out.
+					close_exit(&exec, mini, "dup STDOUT", 1);
+			}
+			close_all_pipes(exec.pipe);
 			// redirections
 			// exec
 			// maybe a single function?
 		}
-		if (exec.pids[i] == -1)
-			perror("fork"); // not returning so waiting is possible
+		close_all_pipes(exec.pipe);
 	}
 	//waiting
 	//freeing
